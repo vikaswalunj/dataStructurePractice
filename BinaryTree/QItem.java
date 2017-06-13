42079f32d8487c7aa63749c50c6c6920a2d61615
vikaswalunj <vikas.k.walunj@gmail.com>
2017-06-13 17:29:30 -0500

    adding binary tree

---
+++ BinaryTree/QItem.java
@@ -0,0 +1,13 @@
+package ds.BinaryTree;
+
+public class QItem {
+	
+	   Node node;
+	   int hd;
+	   public QItem(Node n, int h)
+	   {
+	        node = n;
+	        hd = h;
+	   }
+
+}
---
+++ BinaryTree/TreeUseCases.java
@@ -0,0 +1,48 @@
+package ds.BinaryTree;
+
+public class TreeUseCases {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//**********create sample Tree********//
+		BinaryTree bt = new BinaryTree();
+		bt.root = new Node(1);
+		bt.root.left = new Node(2);
+		bt.root.right = new Node(3);
+		bt.root.left.left = new Node(4);
+		bt.root.left.right = new Node (5);
+		bt.root.right.left = new Node (6);
+		bt.root.right.right = new Node (7);
+		bt.root.left.left.left = new Node(8);
+		bt.root.left.left.right = new Node(9);
+		
+		//bt.PostOrder(bt.root);
+		//bt.printLevelOrder(bt.root);
+		
+		//bt.printkdistanceNode(bt.root, bt.root.left.left, 2);
+		int res = bt.updateTree(bt.root);
+		System.out.println("res=" +res);
+		//bt.height(bt.root);
+		/*print nodes at distance k */
+		//bt.printDistantNodes(bt.root, 5);
+		/*get maximum width*/
+		//bt.getMaxWidth(bt.root);
+		
+		/*clone bt tree*/
+		/*Node cNode = new Node();
+		bt.cloneMyTree(bt.root, cNode);
+		bt.printTree(cNode);*/
+		
+		/*bt.printLevelOrder(bt.root);
+		
+		 test InOrder 
+		bt.InOrderRecur(bt.root);
+		System.out.println("in order with stack");
+		bt.InOrderStack(bt.root);
+		System.out.println("print pre order");
+		bt.PreOrder(bt.root);
+		System.out.println("print post order");
+		bt.PostOrder(bt.root);*/
+	}
+
+}
